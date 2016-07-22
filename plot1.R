90915a0f9a232536dcea4a05347fe7f461442072
cristianocardoso <ccristianocardosoc@hotmail.com>
2016-07-22 13:44:58 -0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,7 +6,6 @@ sourceData$Date <- as.Date(as.character(sourceData$Date), format = "%d/%m/%Y")
 # select only 2 days
 dateData <- subset(sourceData, Date >= "2007-02-01" & Date <= "2007-02-02")
 
-# create new column
 dateData$Global_active_power <- as.numeric(as.character(dateData$Global_active_power))
 
 # Create file (image)
